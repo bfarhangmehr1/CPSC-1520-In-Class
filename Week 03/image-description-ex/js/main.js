55fb3d55d33d45ba882c648471377b0619c19568
bfarhangmehr1 <35229182+bfarhangmehr1@users.noreply.github.com>
2018-01-24 09:31:57 -0700

    final chang on image-description

--- Week 03/image-description-ex/js/main.js
+++ Week 03/image-description-ex/js/main.js
@@ -1,29 +1,27 @@
-// 1) declare my event handelers
+// 1) Declare my event handlers
+function showImage(evt) {
+    // a) Get a reference to the <img class="feature"> element
+    var imgElement = document.querySelector('img.feature');
+    // b) Remove the 'hidden' class from the element
+    imgElement.classList.remove('hidden');
+    // c) Get the href from the <a class="feature link" href="..."> element
+    //    (storing in a local, temporary variable)
+    var imgSrc = linkElement.href;
+    // d) Set the .src attribute's value for the <img>
+    imgElement.src = imgSrc;
+    // e) Set the .alt attribute's value for the <img>
+    imgElement.alt = linkElement.title;
+    // f) Get a reference to the <p class="feature title"></p> element
+    var captionParagraph = document.querySelector('p.feature.title');
+    // g) Put some text in that paragraph
+    captionParagraph.innerHTML = linkElement.title;
 
-function handdleFeatureLink(evt){
-    console.log('shwo image function called')
-    // a) get the refernce to the <img class ="feature"> element
-    var featureImage= document.querySelector('img.feature');
-    
-    // b) remove  the hidden class from the element
-    featureImage.classList.remove('hidden');
-   // c) get the href from the <a class ="feature link" href="..."> element 
-   // (storing in a local temperary variable)    
-    //d) set the .src attribute's value for the <img>
-    featureImage.src= featuerLink.href;
-
-    featureImage.alt=featurelink.title;
-    var captionParagraph=
-    document.querySelector('p.feature.title');
-    captionParagraph.innerHTML= featurelink.title;
+    // h) Stop the default actions of this event (the 'click' on an <a> tag)
     evt.preventDefault();
 }
-// 2) find the element that respond to events
-var featurelink= document.querySelector('a.feature.link');
 
+// 2) Find the elements that respond to events
+var linkElement = document.querySelector('a.feature.link');
 
-//3) "Wire up" the element to the event handlers
-
-featuerLink.addEventListener('click',handleFeatureLinkClick);
-
-
+// 3) "Wire up" the elements to the event handlers
+linkElement.addEventListener('click', showImage);
