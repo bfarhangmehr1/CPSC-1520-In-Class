6794e9ee574be91319ceeb899d3cbefaccf242fd
bfarhangmehr1 <35229182+bfarhangmehr1@users.noreply.github.com>
2018-01-26 08:42:16 -0700

    update string manuplation

--- Week 03/string-manipulations/string-manipulations/js/main.js
+++ Week 03/string-manipulations/string-manipulations/js/main.js
@@ -1,30 +1,25 @@
 var urlTemplate = 'http://placehold.it/:size/:color/:background/';
 var urlQueryStringTemplate = "?text="
 
-
 var textInput = document.querySelector('input[name=placeholderText]');
-console.log(textInput);
 
-function onPlaceholderTextchang(evt){
-    // the .target of the evt is hte object that "expercend " the evt
-    console.log(evt.target.value);// value is the attribute of <input> element
- 
-   var imgElement= document.querySelector('img');
-   var src= imgElement.src;
-   var indexOfEqualSign = img.indexOf('=');
-   var newSrc= src.substring(0,indexOfEqualSign);
-   console.log(newSrc);
-    
-   newSrc=newSrc+ '=' + evt.target.value;
-   imgElement.src=newSrc;
+function onPlaceholderTextChanged(evt) {
+    // The .target of the evt is the object that "experienced" the event
+    console.log(evt.target.value); // .value is an attribute of the <input> element
 
-
+    var imgElement = document.querySelector('img');
+    var src = imgElement.src;
+    var indexOfEqualSign = src.indexOf('=');
+    var newSrc = src.substring(0, indexOfEqualSign);
+    console.log(newSrc);
+    newSrc = newSrc + '=' + evt.target.value;
+    imgElement.src = newSrc;
 }
-//wier up my envt handler
-textInput.addEventListener('change',onPlaceholderTextchang);
+
+// Wire up my event handler
+textInput.addEventListener('change', onPlaceholderTextChanged);
+
 // mini-lesson on string manipulation:
-
-
 
 //  split string into an array
 //  join from array into a string
