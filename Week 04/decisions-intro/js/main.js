fd9d582934faf2622f43fd48df972d01a84a3c4f
bfarhangmehr1 <35229182+bfarhangmehr1@users.noreply.github.com>
2018-01-30 18:35:41 -0700

    update the Week 04 start

--- Week 04/decisions-intro/js/main.js
+++ Week 04/decisions-intro/js/main.js
@@ -1,32 +1,14 @@
-//An anonymose function is a function that does have a name 
-// anonymoses fucntoion are often used when you want to creat a "one-of"
-// function that is being passed into another method/function
-
-  document.querySelector('img.feature')
-     .addEventListener('click',function () {
-		var desc = document.querySelector('p.feature.description');
-	
-		// remove the hidden class IF it is present,
-		// otherwise, add it back!
+// An anonymous function is a function that does not have a name
+// Anonymous functions are often used when you want to create a "one-of"
+// function that is being passed into another method/function.
+document.querySelector('img.feature')
+		.addEventListener('click', function () {
+			var desc = document.querySelector('p.feature.description');
 		
-		
-		if(desc.classList.contains('hidden')){
-			desc.classList.remove('hidden');
-	
-		}else{
-			desc.classList.add('hidden');
-		}
-		// achieve the same result using using string functions
-		//	if(desc.className.indexOf('hidden') >= 0){
-		//	desc.classList.remove('hidden');
-	
-		//}else{
-		//	desc.classList.add('hidden');
-		//}
-		
-	});
+			if (desc.classList.contains('hidden')) {
+				desc.classList.remove('hidden');
+			} else {
+				desc.classList.add('hidden');
+			}
+		});
 
-// 2. create the event listener function
-
-// 3. add event listener
-//featureImage.addEventListener('click', handleFeatureClick);
--- Week 04/thumbnail-resize/js/main.js
+++ Week 04/thumbnail-resize/js/main.js
@@ -1,51 +1,45 @@
-// When a gallery thumbnail size button is clicked, the corresponding size class
-// should be added to the div.thumbnails element. 
+// When a gallery thumbnail size button is clicked, the corresponding 
+// size class should be added to the div.thumbnails element.
+// This will apply the corresponding CSS rule to modify the display of
+// the thumbnail images.
+// In addition to this, the clicked button should be made active
+// (i.e. add the active class).
 
-// This will apply the corresponding CSS rule to  modify the display the of hte thumbnail images. 
+// var liSm = document.querySelector('.thumbnails-sm');
+// var liMd = document.querySelector('.thumbnails-md');
+// var liLg = document.querySelector('.thumbnails-lg');
+var ulContainer = document.querySelector('ul.thumbnail-sizes');
 
-// In addition to this, the clicked button should be made active (i.e. add the active class).
+function thumbnailSizeHandler(evt) {
+    var target = evt.target; // evt.target is the object the event happened on
+    var thumbnails = document.querySelector('div.thumbnails');
 
-//var liSm= document.querySelector('.thumbnails-sm');
-//var liMd= document.querySelector('.thumbnails-md');
-//var liLg= document.querySelector('.thumbnails-lg');
-var ulcontainer = document.querySelector('ul.thumbnails-sizes');
-
-function thumbnaiSizeHandler (evt) {
-    //Too
-    var target = evt.target; // evt.target is the object the event happend on 
-    var thumbnails= document.querySelector('div.thumbnalis');
-    if(target.classList.contains('active')!=true){
+    if(target.classList.contains('active') != true) {
         // update the active <li> element
-       // console.log(target)
-       // console.log('need to swithc the the selected size');
-       document.querySelector('li.active').classList.remove('active');
-       target.classList.add('active');
-    
+        document.querySelector('li.active').classList.remove('active');
+        target.classList.add('active');
 
-       // determine which button or <li> was clicked 
-       if (target.classList.contains('thumbnails-sm')){
-           console.log('swicth size to "sm"');
-           thumbnails.classList.remove('md');
-           thumbnails.classList.remove('lg');
-           thumbnails.classList.add('sm');
-       } else if(target.classList.contains('thumbnails-md')) {
-           console.log('swith size to "md"');
-           thumbnails.classList.remove('sm');
-           thumbnails.classList.remove('lg');
-           thumbnails.classList.add('md');
-       }else{
-        console.log('swith size to "lg"');
-        thumbnails.classList.remove('sm');
-        thumbnails.classList.remove('md');
-        thumbnails.classList.add('lg');
-       }
+        // determine which button/<li> was clicked
+        if(target.classList.contains('thumbnails-sm')) {
+            console.log('switch size to "sm"');
+            thumbnails.classList.remove('md');
+            thumbnails.classList.remove('lg');
+            thumbnails.classList.add('sm');
+        } else if(target.classList.contains('thumbnails-md')) {
+            console.log('switch size to "md"');
+            thumbnails.classList.remove('sm');
+			thumbnails.classList.remove('lg');
+			thumbnails.classList.add('md');
+        } else {
+            console.log('switch size to "lg"');
+            thumbnails.classList.remove('sm');
+			thumbnails.classList.remove('md');
+			thumbnails.classList.add('lg');
+        }
+    }
+}
 
-       }
-    
-    }   
-
-
-//liSm.addEventListener('click',thumbnaiSizeHandler)
-//liMd.addEventListener('click',thumbnaiSizeHandler)
-//liLg.addEventListener('click',thumbnaiSizeHandler)
-ulcontainer.addEventListener('click',thumbnaiSizeHandler);
+// liSm.addEventListener('click', thumbnailSizeHandler);
+// liMd.addEventListener('click', thumbnailSizeHandler);
+// liLg.addEventListener('click', thumbnailSizeHandler);
+ulContainer.addEventListener('click', thumbnailSizeHandler);
