6c97215d9c67c6326119e5f85b6985a61dbe5079
bfarhangmehr1 <35229182+bfarhangmehr1@users.noreply.github.com>
2018-01-31 09:47:53 -0700

    decision files exercise

--- Week 04/Decisions_FILES/image_description2/js/main.js
+++ Week 04/Decisions_FILES/image_description2/js/main.js
@@ -1,11 +1,22 @@
 var featureLink = document.querySelector('a.feature');
+var LinkText = document.querySelector('a.feature');
+console.log(LinkText);
 
 function featureLinkHandler(evt) {
     var featureImage = document.querySelector('img.feature');
-    featureImage.src = featureLink.href;
-    featureImage.classList.remove('hidden');
-
+    if(featureImage.classList.contains('hidden')!==true){ 
+        LinkText.innerHTML="Hide Barcelona";       
+        featureImage.src = featureLink.href; 
+        //featureImage.classList.remove('hidden'); 
+            
+        
+        
+    }else{        
+                      
+        featureImage.classList.add('hidden'); 
+        LinkText.innerHTML="View Barcelona";       
+        //featureImage.src='';      
+    }    
     evt.preventDefault();
 }
-
 featureLink.addEventListener('click', featureLinkHandler);
