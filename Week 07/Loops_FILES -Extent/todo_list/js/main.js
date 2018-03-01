9346fbea54e5443fce7635969f43383d92719da7
bfarhangmehr1 <35229182+bfarhangmehr1@users.noreply.github.com>
2018-03-01 13:47:32 -0700

    to do list extend

--- Week 07/Loops_FILES -Extent/todo_list/js/main.js
+++ Week 07/Loops_FILES -Extent/todo_list/js/main.js
@@ -1,17 +1,13 @@
 // Enter JavaScript for the exercise here...
+var checks=document.querySelectorAll('[name=message]');
 
-
-var myform = document.querySelector('form.myform');
-
-myform.addEventListener('submit', function (evt){ 
-    var checks = document.querySelectorAll('[name=message]');
-    var myform = evt.target;
+checks.addEventListener('change',function (evt){ 
+    var checkbox= evt.target;
+    var myform = document.querySelector('form.myform');       
     var input= myform.elements.partialstringId;
-
     var idx = 0;
-    while (idx < checks.length) {
-    if (checks.values().match(input.value)) {
-     evt.preventDefault();
+    while (idx < checks.length) {  
+    if (input.value.toLowerCase().indexOf(checks.item(idx).nodeValue.toLowerCase())>=0){    
     checks[idx].classList.add('.stripe');
     } 
     idx = idx + 1;
