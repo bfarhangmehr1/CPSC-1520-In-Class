8f7ea7c2cc9a50fee00facde148e0b6da31a55f2
bfarhangmehr1 <35229182+bfarhangmehr1@users.noreply.github.com>
2018-03-02 09:20:24 -0700

    update the file

--- Week 07/Loops_FILES -Extent/todo_list/js/main.js
+++ Week 07/Loops_FILES -Extent/todo_list/js/main.js
@@ -1,15 +1,19 @@
 // Enter JavaScript for the exercise here...
-var checks=document.querySelectorAll('[name=message]');
+//var checks=document.querySelectorAll('[name=message]');
+var myform = document.querySelector('form.myform');
 
-checks.addEventListener('change',function (evt){ 
-    var checkbox= evt.target;
-    var myform = document.querySelector('form.myform');       
+myform.addEventListener('submit',function (evt){ 
+    var formElements= evt.target;
+    //var myform = document.querySelector('form.myform');
+    var checks=document.querySelectorAll('td');       
     var input= myform.elements.partialstringId;
-    var idx = 0;
-    while (idx < checks.length) {  
-    if (checks.item(idx).nodeValue.toLowerCase().indexOf(input.value.toLowerCase())> -1){    
-    checks[idx].classList.add('.stripe');
+    var idx = 1;
+    while (idx <= checks.length) {  
+    if (checks.item(idx).innerHTML.toLowerCase().indexOf(input.value.toLowerCase())>-1){    
+    checks[idx].classList.add('stripe');
+    checks[idx-1].classList.add('stripe');    
+    evt.preventDefault();
     } 
-    idx = idx + 1;
+    idx = idx + 2;
     }
    }); 
